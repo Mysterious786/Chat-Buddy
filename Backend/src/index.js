e3c57595e522d40fbd7119335b3c4c1d88580fe8
DESKTOP-GUGSNQ4 <saqlainzarjisansari@gmail.com>
2025-01-27 16:08:33 +0530

    feat: profile pic functionality implemented

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser()); // allows to parse the cookie
+app.use(cors(
+   { origin: "http://localhost:5173",
+    credentials:true,
+   }
+));
 app.use("/api/auth", authRoutes);
 app.use("/api/message",messageRoutes);
 

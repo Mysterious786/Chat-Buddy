0345e13d40a915579562f58c7f15609e7ca4cc8c
DESKTOP-GUGSNQ4 <saqlainzarjisansari@gmail.com>
2025-01-27 12:38:13 +0530

    feat: Backend routes completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser()); // allows to parse the cookie
 app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running successfully on port: " + PORT);
